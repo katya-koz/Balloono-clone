b6fcb0f67d12bd26600e3664d8e60de309a29ad6
katya-koz <119365237+katya-koz@users.noreply.github.com>
2022-11-28 17:39:06 -0500

    Update config.js

--- Balloono/firebase/config.js
+++ Balloono/firebase/config.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDHnHYC35lGXADwFrXYNQK9ju8inJMtWjM",
-  authDomain: "balloono2.firebaseapp.com",
-  databaseURL: "https://balloono2-default-rtdb.firebaseio.com",
-  projectId: "balloono2",
-  storageBucket: "balloono2.appspot.com",
-  messagingSenderId: "824777434452",
-  appId: "1:824777434452:web:1bb906557fffaf1f3554c1"
+// create new project in firebase, and paste config here
 };
 
 export { firebaseConfig };
